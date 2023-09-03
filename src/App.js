7c2b681a3091b87200d45da88e7644a3d70d695e
GOPALAKRISHNAYM <49530827+GOPALAKRISHNAYM@users.noreply.github.com>
2023-09-03 13:57:19 +0530

    Updated image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import myImage from './Assets/stayTuned.PNG';
 
 function App() {
+  const style = {
+    paddingTop: '9.5%',
+    paddingLeft: '20%'
+  }
+
+  const parentCss = {
+    backgroundColor: '#060524',
+    height: '100vh'
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header></Header> */}
+
+      {/* <Grid container spacing={2}>
+        <Grid item xs={8}></Grid>
+        <Grid item xs={4}>
+          <Login></Login>
+        </Grid>
+      </Grid> */}
+      <img src={myImage} width="60%"
+        height="400" style={style} alt="Description of the image" />
     </div>
   );
 }
